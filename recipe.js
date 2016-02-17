ed3e41884d7b5a6cd90bfdbbe7dcd2f709e1a48f
Mitul Mistry <mitul.mistry1@gmail.com>
2016-02-17 04:19:23 +0000

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function addIngredient(recipe_object, ingredient, amount) {
+  recipe_object[ingredient] = amount;
+  return recipe_object;
+}
+
+function removeIngredient(recipe_object, ingredient) {
+  delete recipe_object[ingredient];
+  return recipe_object;
+}
+
+function updateIngredient(recipe_object, ingredient, amount) {
+  recipe_object[ingredient] = amount;
+  return recipe_object;
+}
+
+function readRecipe(recipe_object) {
+  for (var key in recipe_object) {
+    console.log("this recipe calls for " + recipe_object[key] + " of " + key);
+  }
+}
